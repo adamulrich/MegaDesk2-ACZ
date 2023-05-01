f6c133fd668f037da9b74f6c825c3ad9d2caa29b
adamulrich <adamulrich@hotmail.com>
2023-04-30 23:13:07 -0700

    adding form requirements

--- MegaDesk/DeskQuote.cs
+++ MegaDesk/DeskQuote.cs
@@ -8,7 +8,17 @@ namespace MegaDesk
 {
     public class DeskQuote
     {
-        readonly Dictionary<material, float> materialCost = new Dictionary<material, float>()
+
+        int BASE_COST = 200;
+        int SURFACE_AREA_COST_INCREASE_SIZE = 1000;
+        int EXTRA_SURFACE_AREA_COST = 1;
+        int DRAWER_COST = 50;
+        int SMALL_SIZE = 0;
+        int MEDIUM_SIZE = 1;
+        int LARGE_SIZE = 2;
+
+
+        Dictionary<material, int> materialCost = new Dictionary<material, int>()
         {
             { material.Laminate, 100 },
             { material.Oak, 200 },
@@ -17,7 +27,7 @@ namespace MegaDesk
             { material.Veneer, 125 }
         };
 
-        readonly Dictionary<productionTime, int[]> rushCost = new Dictionary<productionTime, int[]>()
+        Dictionary<productionTime, int[]> rushCost = new Dictionary<productionTime, int[]>()
         {
             {productionTime.rush3Days, new int[] {60,70,80 } },
             {productionTime.rush5Days, new int[] {40,50,60 } },
@@ -62,7 +72,7 @@ namespace MegaDesk
             }
         }
 
-        public dateTime Date
+        public DateTime Date
         {
             get => default;
             set
@@ -70,9 +80,33 @@ namespace MegaDesk
             }
         }
 
-        public void caclulatePrice()
+        public void calculatePrice()
         {
-            throw new System.NotImplementedException();
+            // calculate size of desk and drawers and material
+            int deskPrice = BASE_COST + 
+                            this.desk.drawer_count * DRAWER_COST +
+                            materialCost[this.desk.material];
+
+            // if large, then add extra
+            int deskSize = this.desk.depth * this.desk.width;
+            if (deskSize > SURFACE_AREA_COST_INCREASE_SIZE)
+            {
+                deskPrice += (deskSize - SURFACE_AREA_COST_INCREASE_SIZE) * EXTRA_SURFACE_AREA_COST;
+            }
+
+            // add production cost based on size
+            int rushIndex = SMALL_SIZE;
+            if (deskSize > 2000)
+            {
+                rushIndex = LARGE_SIZE;
+            }
+            else if (deskSize > 1000)
+            {
+                rushIndex = MEDIUM_SIZE;
+            }
+
+            deskPrice += rushCost[this.productionTime][rushIndex];
+
         }
 
         public void saveQuote()
