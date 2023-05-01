350d34749bc1716c798912a8d9c90bda030198ed
Adam Ulrich <adamulrich@hotmail.com>
2023-05-01 13:16:56 -0700

    add icon

--- MegaDesk/MainMenu.Designer.cs
+++ MegaDesk/MainMenu.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
             this.btn_searchQuotes = new System.Windows.Forms.Button();
             this.btn_exit = new System.Windows.Forms.Button();
             this.btn_viewQuote = new System.Windows.Forms.Button();
@@ -38,12 +39,12 @@
             // 
             // btn_searchQuotes
             // 
-            this.btn_searchQuotes.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btn_searchQuotes.Image = global::MegaDesk.Properties.Resources.search_FILL0_wght400_GRAD0_opsz48;
             this.btn_searchQuotes.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_searchQuotes.Location = new System.Drawing.Point(28, 156);
+            this.btn_searchQuotes.Location = new System.Drawing.Point(19, 97);
+            this.btn_searchQuotes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_searchQuotes.Name = "btn_searchQuotes";
-            this.btn_searchQuotes.Size = new System.Drawing.Size(247, 76);
+            this.btn_searchQuotes.Size = new System.Drawing.Size(165, 49);
             this.btn_searchQuotes.TabIndex = 2;
             this.btn_searchQuotes.Text = "Search Quotes";
             this.btn_searchQuotes.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -51,39 +52,39 @@
             // 
             // btn_exit
             // 
-            this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btn_exit.Image = global::MegaDesk.Properties.Resources.logout_FILL0_wght400_GRAD0_opsz48;
             this.btn_exit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_exit.Location = new System.Drawing.Point(28, 389);
+            this.btn_exit.Location = new System.Drawing.Point(19, 239);
+            this.btn_exit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_exit.Name = "btn_exit";
-            this.btn_exit.Size = new System.Drawing.Size(247, 83);
-            this.btn_exit.TabIndex = 3;
+            this.btn_exit.Size = new System.Drawing.Size(165, 54);
+            this.btn_exit.TabIndex = 4;
             this.btn_exit.Text = "Exit";
             this.btn_exit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btn_exit.UseVisualStyleBackColor = true;
             // 
             // btn_viewQuote
             // 
-            this.btn_viewQuote.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btn_viewQuote.Image = global::MegaDesk.Properties.Resources.receipt_long_FILL0_wght400_GRAD0_opsz48;
             this.btn_viewQuote.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_viewQuote.Location = new System.Drawing.Point(28, 272);
+            this.btn_viewQuote.Location = new System.Drawing.Point(19, 168);
+            this.btn_viewQuote.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_viewQuote.Name = "btn_viewQuote";
-            this.btn_viewQuote.Size = new System.Drawing.Size(247, 76);
-            this.btn_viewQuote.TabIndex = 4;
+            this.btn_viewQuote.Size = new System.Drawing.Size(165, 49);
+            this.btn_viewQuote.TabIndex = 3;
             this.btn_viewQuote.Text = "View Quotes";
             this.btn_viewQuote.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btn_viewQuote.UseVisualStyleBackColor = true;
             // 
             // btn_addQuote
             // 
-            this.btn_addQuote.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btn_addQuote.Image = global::MegaDesk.Properties.Resources.add_circle_FILL0_wght400_GRAD0_opsz48;
             this.btn_addQuote.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_addQuote.Location = new System.Drawing.Point(28, 40);
+            this.btn_addQuote.Location = new System.Drawing.Point(19, 26);
+            this.btn_addQuote.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_addQuote.Name = "btn_addQuote";
-            this.btn_addQuote.Size = new System.Drawing.Size(247, 76);
-            this.btn_addQuote.TabIndex = 5;
+            this.btn_addQuote.Size = new System.Drawing.Size(165, 49);
+            this.btn_addQuote.TabIndex = 1;
             this.btn_addQuote.Text = "Add Quote";
             this.btn_addQuote.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btn_addQuote.UseVisualStyleBackColor = true;
@@ -91,7 +92,8 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::MegaDesk.Properties.Resources.desk_icon;
-            this.pictureBox1.Location = new System.Drawing.Point(322, 80);
+            this.pictureBox1.Location = new System.Drawing.Point(218, 26);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(512, 268);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -100,14 +102,17 @@
             // 
             // MainMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1140, 584);
+            this.ClientSize = new System.Drawing.Size(760, 320);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btn_addQuote);
             this.Controls.Add(this.btn_viewQuote);
             this.Controls.Add(this.btn_exit);
             this.Controls.Add(this.btn_searchQuotes);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
             this.Name = "MainMenu";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "MainMenu";
